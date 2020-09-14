7132e2c003fdd2d962ad83b622091d96fdb00d44
Richard Tudose <tudose.richard@gmail.com>
2020-09-14 11:01:25 -0600

    Adding code outline and documentation for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -9,35 +9,89 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief Main file of the project to perform statistical analytics
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * The functions of this program perform statistics like finding the 
+ * maximum, minimum, mean, and median of the data set and also 
+ * orders the array from large to small.
+ * @author Richard Tudose
+ * @date 4/09/2020
  *
  */
 
-
-
 #include <stdio.h>
 #include "stats.h"
+#include <math.h>
 
-/* Size of the Data Set */
-#define SIZE (40)
+/* size of the Data Set */
+#define SIZE 40
 
 void main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+  unsigned char test [SIZE] = {34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+                                7,  87, 250, 230,  99,   3, 100,  90 };
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+  
+  /*Variables declarations*/
+  float mean = 0, median = 0, min, max;
+  
+  /* Statistics and Printing Functions */
+  mean = find_mean ( test, SIZE );
+  median = find_median ( test, SIZE );
+  max = find_maximum ( test, SIZE );
+  min = find_minimum ( test, SIZE );
+  print_statistics ( mean, max, min, median );
+  sort_array( test, SIZE );
+  print_array( test, SIZE );
+}
+
+/* Printing function */
+void print_statistics ( float mean, float max, float min, float median ) {
+  	
+}
+
+/* Calculating the mean value of the array */
+float find_mean ( unsigned char a[], unsigned int n ) {
+
+  return mean ;
+  
+} 
+
+/* Finding the maximum value of the array */
+float find_maximum ( unsigned char a[], unsigned int n ) {
+
+  return max;
+  
+}
+
+/* Finding the minimum value of the array */
+float find_minimum ( unsigned char a[], unsigned int n ) {
+
+  return min;
+  
+}
+
+/* Finding the median value of the array */
+float find_median ( unsigned char a[], unsigned int n ) {
+  
+  return median ;
+  
+} 
+
+/* Sorting the array from big to small ( e.g. 5 3 1 ) */
+void sort_array ( unsigned char a[], unsigned int n ) {
+ 
+ 
+}
+
+/* Printing the array */
+void print_array ( unsigned char a[], unsigned int n ) {
 
 }
 
-/* Add other Implementation File Code Here */
+
+
--- stats.h
+++ stats.h
@@ -9,32 +9,108 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h
+ * @brief Header of the project: statistics of an array
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * This file contains the declarations and documentation for the functions from 
+ * the stats.c file
+ * @author Richard Tudose
+ * @date 4/09/2020
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+void print_statistics ( float mean, float max, float min, float median );
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Prints all the calculated statistics
  *
- * <Add Extended Description Here>
+ * A function that prints the statistics 
+ * of an array including minimum, maximum, mean, and median.
+ 
+ * @param mean - the calculated mean value of the array
+ * @param max - the maximum found value of the array
+ * @param min - the minimum found value of the array
+ * @param median - the median value of the array
+ */
+ 
+void print_array ( unsigned char test[], unsigned int SIZE );
+
+/**
+ * @brief Prints a given aray to the screen
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * Given an array of data and a length, prints the array to the screen
  *
- * @return <Add Return Informaiton here>
+ * @param test[] - the given array
+ * @param SIZE - the size of the array we want to print
  */
 
+float find_median(unsigned char test[], unsigned int SIZE);
+
+/**
+ * @brief Calculates the median of the array
+ *
+ * Given an array of data and a length, returns the median value
+ *
+ * @param test[] - the given array
+ * @param SIZE - the size of the array 
+ *
+ * @return - The median value of the array
+ */
+ 
+float find_mean ( unsigned char test[], unsigned int SIZE );
+
+/**
+ * @brief Calculates the mean value of the array
+ *
+ * Given an array of data and a length, returns the mean
+ *
+ * @param test[] - the given array
+ * @param SIZE - the size of the array
+ *
+ * @return - The mean value of the array
+ */
+ 
+float find_maximum ( unsigned char test[], unsigned int SIZE );
+
+/**
+ * @brief Finds the maximum value of an array
+ *
+ * Given an array of data and a length, returns the maximum
+ *
+ * @param test[] - the given array
+ * @param SIZE - the size of the array
+ *
+ * @return The maximum value of the array
+ */ 
+ 
+float find_minimum ( unsigned char test[], unsigned int SIZE );
+
+/**
+ * @brief Finds the minimum value of an array
+ *
+ * Given an array of data and a length, returns the minimum
+ *
+ * @param test[] - the given array
+ * @param SIZE - the size of the array
+ *
+ * @return The minimum value of the array
+ */ 
+ 
+void sort_array ( unsigned char test[], unsigned int SIZE );
+
+/**
+ * @brief Sorts an array from largest to smallest
+ *
+ * Given an array of data and a length, sorts the 
+ * array from largest to smallest. (The zeroth 
+ * Element should be the largest value, and the last
+ * element (n-1) should be the smallest value. )
+ *
+ * @param test[] - the given array
+ * @param SIZE - the size of the array
+ */
+  
 
 #endif /* __STATS_H__ */
